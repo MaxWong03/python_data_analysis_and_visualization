c711e85a79a5bd1aebf610ecd5cba2ae9d638610
MaxWong03 <madmaxmmajournal@gmail.com>
2022-04-15 11:34:04 -0700

    commit intro to analytics note

---
+++ intro_to_analytics.py
@@ -0,0 +1,21 @@
+'''
+What is analytics?
+  - The process of taking raw data and extracting actionable insight
+
+When starting a new analytic project, you are given data and a question you want to answer ...
+
+The analytics process
+  1) Processing the data
+    - Read data into a system for processing
+  2) Describing the data
+    - Calculate various statistics and counts to describe the data
+  3) Cleaning the data
+    - 2) tends to reveal potentail issues and will require cleaning the data
+  4) Visualizing the data
+    - Go back and do a deeper dive into describing and understanding data 
+
+Importance of analytics
+  - Peel back the layers of data to try and answer your question or questions of interest
+  - Allow for start with no knowledge and a bunch of data to data-driven understanding
+
+'''
